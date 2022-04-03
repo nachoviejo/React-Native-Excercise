e8d67cc5564c38559046afce885e75104adfc3d8
Ignacio Viejo <ignacionachoviejo@gmail.com>
2022-04-03 20:19:52 -0300

    ip fixed

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,10 @@
+import { Platform } from "react-native"
+
+const IP = Platform.OS === 'ios' ? '127.0.0.1' : '10.0.2.2'
 export const tasksUrl = () => {
-  return `http://127.0.0.1:3001/tasks` 
+  return `http://${IP}:3001/tasks` 
 }
 
 export const taskUrl = (id: number) => {
-  return `http://127.0.0.1:3001/tasks/${id}` 
+  return `http://${IP}:3001/tasks/${id}` 
 }
