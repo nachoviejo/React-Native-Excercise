3273850d8ab55c4f470e403727ee4c098ac1b8f3
Ignacio Viejo <ignacionachoviejo@gmail.com>
2022-04-04 09:45:18 -0300

    IP and PORT config

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,10 +1,12 @@
 import { Platform } from "react-native"
 
 const IP = Platform.OS === 'ios' ? '127.0.0.1' : '10.0.2.2'
+const PORT = '3001'
+
 export const tasksUrl = () => {
-  return `http://${IP}:3001/tasks` 
+  return `http://${IP}:${PORT}/tasks` 
 }
 
 export const taskUrl = (id: number) => {
-  return `http://${IP}:3001/tasks/${id}` 
+  return `http://${IP}:${PORT}/tasks/${id}` 
 }
